131a90cff25e82144ca83b111324bb1775efd1d4
Ivann Alcantara <ivann850@gmail.com>
2025-03-03 21:32:23 -0500

    started favlinks project files

---
+++ app/components/Table.jsx
@@ -0,0 +1,20 @@
+function Table(){
+    
+    return (
+        <table>
+            {/* Table Header*/}
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>URL</th>
+                </tr>
+
+            </thead>
+
+
+        </table>
+
+    )
+
+}
+export default Table
